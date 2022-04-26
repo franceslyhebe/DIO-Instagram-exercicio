770d5f95ddf43c13d0526eb052938bdf2bec5aa8
Hebe <f.francesly@gmail.com>
2022-04-26 15:43:43 -0300

    Desafio DIO

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # DIO-Instagram-exercicio
 Exercício de recriar página de login do Instagram.
+
+Recriei a página do dia 26/04/2022, a mostrada em aula apresenta algumas diferenças, como o uso da imagem do perfil.
+
---
+++ main.js
@@ -0,0 +1,26 @@
+let slide_content = document.querySelector('#slide-content')
+
+let slide_index = 0
+
+slide = () => {
+    let slide_items = slide_content.querySelectorAll('img')
+    slide_items.forEach(e => e.classList.remove('active'))
+    slide_index = slide_index + 1 === slide_items.length ? 0 : slide_index + 1
+    slide_items[slide_index].classList.add('active')
+}
+
+setInterval(slide, 2000)
+
+// animate input
+document.querySelectorAll('.animate-input').forEach(e => {
+    let input = e.querySelector('input')
+    
+    input.onkeyup = () => {
+        if (input.value.trim().length > 0) {
+            e.classList.add('active')
+        } else {
+            e.classList.remove('active')
+        }
+    }
+
+})
